87fbf4234b1a093ac8b135c8a631a40dfafee275
patrickhaetti <patrickhaetti@gmx.de>
2023-11-30 12:54:51 +0100

    chapter 26

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
 
 
@@ -10,7 +10,13 @@ monthly_challenges = {
 
 
 def monthly_challenge_by_number(request, month):
-    return HttpResponse(month)
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+    
+    actual_month = months[month-1]
+    return HttpResponseRedirect("/challenges/"+actual_month)
 
 
 def monthly_challenge(request, month):
