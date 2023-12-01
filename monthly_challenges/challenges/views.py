ded10779aaf5b6ea8e95c654e575389c569248bd
patrickhaetti <patrickhaetti@gmx.de>
2023-12-01 11:20:26 +0100

    chapter 35 + 36

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -34,6 +34,9 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return render(request, "challenges/challenge.html")
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text,
+            "month": month
+        })
     except:
         return HttpResponseNotFound("<h1>Not supported</h1>")
