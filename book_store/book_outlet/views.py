121063baf3f10fa72f1924884efb847d31962335
patrickhaetti <patrickhaetti@gmx.de>
2024-01-03 16:30:17 +0100

    chapter 88: - specific page for each database query

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
 
 from .models import Book
 
@@ -9,4 +10,20 @@ def index(request):
     return render(request, "book_outlet/index.html", 
                   {
                       "books": books
+                  })
+
+
+def book_detail(request, id):
+    # try:
+    #     book = Book.objects.get(pk=id)
+    # except:
+    #     raise Http404()
+    book = get_object_or_404(Book, pk=id)
+
+    return render(request, "book_outlet/book_detail.html",
+                  {
+                      "title": book.title,
+                      "author": book.author,
+                      "is_bestselling": book.is_bestselling,
+                      "rating": book.rating
                   })
