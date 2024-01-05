7e35ce178e035f407acab667d1cbcca3393e03df
patrickhaetti <patrickhaetti@gmx.de>
2024-01-05 16:55:50 +0100

    chapter 119: convert to Model based. - add Post model

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField()
+    slug = models.SlugField(unique=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
