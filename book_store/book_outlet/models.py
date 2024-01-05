c33e11baf3a0e8b8e8d0d9a9175a49d4c506e583
patrickhaetti <patrickhaetti@gmx.de>
2024-01-05 12:43:39 +0100

    chapter 113: - add Many to Many relation Model to Admin

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -8,6 +8,12 @@ from django.utils.text import slugify
 class Country(models.Model):
     name = models.CharField(max_length=80)
     code = models.CharField(max_length=2)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Countries"
 
 class Address(models.Model):
     street = models.CharField(max_length=80)
