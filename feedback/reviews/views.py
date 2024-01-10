302872568feac040e0c1a0db39e79e7bca03a7d5
patrickhaetti <patrickhaetti@gmx.de>
2024-01-10 08:03:36 +0100

    chapter 137 - form validation (hard coded bad variant as an example)

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -6,10 +6,16 @@ from django.shortcuts import render
 def review(request):
     if request.method == 'POST':
         entered_username = request.POST['username']
+
+        if entered_username == "":
+            return render(request, "reviews/review.html",{
+                "has_error": True
+            })
         print(entered_username)
         return HttpResponseRedirect("/thank-you")
-    else:
-        return render(request, "reviews/review.html")
+    return render(request, "reviews/review.html")
 
 def  thank_you(request):
-    return render(request, "reviews/thank_you.html")
+    return render(request, "reviews/thank_you.html",{
+                "has_error": False
+            })
