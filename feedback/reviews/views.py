0653655a97dd87542be53f14f1a961cc13a4e6df
patrickhaetti <patrickhaetti@gmx.de>
2024-01-10 11:02:50 +0100

    chapter 138:     user_name = forms.CharField() - create form class - use it in views

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,19 +1,21 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+from .forms import ReviewForm
+
 # Create your views here.
 
 def review(request):
     if request.method == 'POST':
-        entered_username = request.POST['username']
-
-        if entered_username == "":
-            return render(request, "reviews/review.html",{
-                "has_error": True
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect("/thank-you")
+    
+    form = ReviewForm()
+    return render(request, "reviews/review.html",{
+                "form": form
             })
-        print(entered_username)
-        return HttpResponseRedirect("/thank-you")
-    return render(request, "reviews/review.html")
 
 def  thank_you(request):
     return render(request, "reviews/thank_you.html",{
