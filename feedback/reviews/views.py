db817832672342c0871e82fcb1c2f71c0f41e6b8
patrickhaetti <patrickhaetti@gmx.de>
2024-01-11 11:10:34 +0100

    chapter 156: - use ListView instead of TemplateView for displaying db entries directly - use context_object_name for naming data passed to frontend - adjusting querying logic by using get_queryset with filter

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,8 +1,10 @@
 from typing import Any
+from django.db.models.query import QuerySet
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 
 from .models import Review
 from .forms import ReviewForm
@@ -37,14 +39,16 @@ class ThankYou(TemplateView):
         context["message"] = "This works!"
         return context
 
-class ReviewListView(TemplateView):
+class ReviewListView(ListView):
     template_name = "reviews/review_list.html"
+    model = Review
+    context_object_name = "reviews"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        reviews = Review.objects.all()
-        context["reviews"] = reviews
-        return context
+    # adjusting querying logic
+    def get_queryset(self):
+        base_query = super().get_queryset()
+        data = base_query.filter(rating__gt=2)
+        return data
 
 class SigleReviewView(TemplateView):
     template_name = "reviews/single_review.html"
