c6d6e7ea80a8a3e4271956d5d7e5f3f76e61dc73
patrickhaetti <patrickhaetti@gmx.de>
2024-01-11 11:43:16 +0100

    chapter 160: - CreateView creates automatically data - form class can be deleted and only model is used, but labels /erros are not possible then so we keep it as form_class = ReviewForm - saving data is also possible with CreateView

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -5,21 +5,18 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import FormView
+from django.views.generic.edit import CreateView
 
 from .models import Review
 from .forms import ReviewForm
 
 # Create your views here.
 
-class ReviewView(FormView):
+class ReviewView(CreateView):
+    model = Review
     form_class = ReviewForm
     template_name = "reviews/review.html"
     success_url = "/thank-you"
-
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
 
 # use TemplateView as specific view for templates
 class ThankYou(TemplateView):
