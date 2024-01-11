dd29486a4227a197e189591b6f094ddd7748b9ed
patrickhaetti <patrickhaetti@gmx.de>
2024-01-11 11:20:15 +0100

    chapter 157: - use DetailView in SingleReviewView

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Review
 from .forms import ReviewForm
@@ -50,12 +50,6 @@ class ReviewListView(ListView):
         data = base_query.filter(rating__gt=2)
         return data
 
-class SigleReviewView(TemplateView):
+class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        review_id = kwargs["id"]
-        selected_review = Review.objects.get(pk=review_id)
-        context["review"] = selected_review
-        return context    
+    model = Review
