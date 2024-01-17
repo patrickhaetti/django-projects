4e19a951ae59488515296444fefdbd0ae68af876
patrickhaetti <patrick.haetti@gmail.com>
2024-01-17 14:58:03 +0100

    chapter 193 - CSS not working as in tutorial: - Comment Form Validation Styles - add css -{% if form_field.errors %}invalid{% endif %}  not working

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -37,29 +37,29 @@ class PostListView(ListView):
 
 
 class SinglePostView(View):
-
     def get(self, request, slug):
         post = Post.objects.get(slug=slug)
         context = {
-            "post": post,
-            "post_tags": post.tags.all(),
-            "comment_form": CommentForm()
+          "post": post,
+          "post_tags": post.tags.all(),
+          "comment_form": CommentForm()
         }
         return render(request, "blog/post_detail.html", context)
-    
+
     def post(self, request, slug):
-        comment_form = CommentForm(request.POST) # CommentForm contains submitted data
+        comment_form = CommentForm(request.POST)
         post = Post.objects.get(slug=slug)
 
         if comment_form.is_valid():
-            comment = comment_form.save(commit=False)
-            comment.post = post
-            comment.save()
-            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
-        
+          comment = comment_form.save(commit=False)
+          comment.post = post
+          comment.save()
+
+          return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+
         context = {
-            "post": post,
-            "post_tags": post.tags.all(),
-            "comment_form": CommentForm()
-        }        
+          "post": post,
+          "post_tags": post.tags.all(),
+          "comment_form": comment_form
+        }
         return render(request, "blog/post_detail.html", context)
