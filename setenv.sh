f4bec83528ed665370e3bee04d1bbac29ed2a1b0
patrickhaetti <patrickhaetti@gmx.de>
2024-01-25 10:28:08 +0100

    add SECRET_KEY

---
+++ setenv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export DEBUG=True
+export SECRET_KEY=''
+export APP_HOST='localhost'
+export ENGINE=''
+export NAME=''
+export USER=''
+export PASSWORD=''
+export HOST=''
+export PORT=''
